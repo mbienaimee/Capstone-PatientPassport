dcf97145339cc2894eb586f71fe1dc63693ec904
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-11-04 15:53:48 +0200

    linking database

--- openmrs-patient-passport-module/omod/src/main/java/org/openmrs/module/patientpassport/PatientPassportModuleActivator.java
+++ openmrs-patient-passport-module/omod/src/main/java/org/openmrs/module/patientpassport/PatientPassportModuleActivator.java
@@ -1,37 +1,43 @@
 package org.openmrs.module.patientpassport;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.BaseModuleActivator;
 
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
 public class PatientPassportModuleActivator extends BaseModuleActivator {
+    
+    private static final Log log = LogFactory.getLog(PatientPassportModuleActivator.class);
 
     /**
      * @see org.openmrs.module.ModuleActivator#willStart()
      */
     public void willStart() {
-        System.out.println("Starting Patient Passport Module");
+        log.info("🚀 Starting Patient Passport Module");
     }
 
     /**
      * @see org.openmrs.module.ModuleActivator#started()
      */
     public void started() {
-        System.out.println("Patient Passport Module started");
+        log.info("✅ Patient Passport Module started");
+        log.info(" Data Flow: OpenMRS → Patient Passport");
+        log.info("📡 Use REST API endpoints to send observations to Patient Passport");
     }
 
     /**
      * @see org.openmrs.module.ModuleActivator#willStop()
      */
     public void willStop() {
-        System.out.println("Stopping Patient Passport Module");
+        log.info("⏸️ Stopping Patient Passport Module");
     }
 
     /**
      * @see org.openmrs.module.ModuleActivator#stopped()
      */
     public void stopped() {
-        System.out.println("Patient Passport Module stopped");
+        log.info("⏹️ Patient Passport Module stopped");
     }
 }
